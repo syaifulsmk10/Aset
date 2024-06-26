b6667b5800a32b58f1f07a6a4a544c41e2fc713a
syaifulsmk10 <syaifulnazar47@gmail.com>
2024-06-26 08:47:53 +0700

    update validation department andposition

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,44 +4,78 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DepartmentController extends Controller
 {
+    
     public function index(){
-        $department = Department::all();
+        $Department = Department::all();
         return response()->json([
-            "data" =>  $department
+            "data" =>  $Department
         ]);
     }
 
+    
 
     public function create(Request $request){
-        department::create([
-            $request->name,
-        ]);
+         $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+    ]);
+     if ($validator->fails()) {
         return response()->json([
-            "message" => "success create department"
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()
+        ], 422);
+    }
+        Department::create($request->all());
+        return response()->json([
+            "message" => "success create Department"
         ]);
     }
 
     public function update(Request $request, $id){
-        $department = department::find($id);
+
+          $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+    ]);
+     if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()
+        ], 422);
+    }
+
+        $Department = Department::find($id);
+        if (!$Department) {
+            return response()->json([
+                "message" => "Department Not Found"
+            ]);
+        }
+
         if ($request->has('name')) {
-        $$department->name = $request->name;
+        $Department->name = $request->name;
+        $Department->save();
     }
 
 
         return response()->json([
-            "message" => "sucess updae department"
+            "message" => "sucess updae Department"
         ]);
     }
 
     public function delete($id){
-        $department = department::find($id);
-        $department->delete();
+        $Department = Department::find($id);
+
+         if (!$Department) {
+            return response()->json([
+                "message" => "Department Not Found"
+            ]);
+        }
+        $Department->delete();
 
         return response()->json([
-            "message" => "success delete department"
+            "message" => "success delete Department"
         ]);
     }
 }
--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Position;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PositionController extends Controller
 {
@@ -16,18 +17,47 @@ class PositionController extends Controller
 
 
     public function create(Request $request){
-        position::create([
-            $request->name,
-        ]);
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+
+    ]);
+     if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()
+        ], 422);
+    }
+    
+        Position::create($request->all());
         return response()->json([
             "message" => "success create position"
         ]);
     }
 
     public function update(Request $request, $id){
-        $position = position::find($id);
+        $position = Position::find($id);
+         $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+     
+    ]);
+     if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()
+        ], 422);
+    }
+
+
+        if(!$position){
+            return response()->json([
+            "message" => "Position Not Found"
+        ]);
+
+        }
         if ($request->has('name')) {
         $position->name = $request->name;
+        $position->save();
     }
 
 
@@ -38,6 +68,11 @@ class PositionController extends Controller
 
     public function delete($id){
         $position = position::find($id);
+        if (!$position) {
+            return response()->json([
+                "message" => "Position Not Found"
+            ]);
+        }
         $position->delete();
 
         return response()->json([
