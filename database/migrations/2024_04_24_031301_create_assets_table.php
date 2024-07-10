af609a9d8ac7d8804395096267b1375c486e3073
syaifulsmk10 <syaifulnazar47@gmail.com>
2024-07-10 11:11:13 +0700

    new

--- database/migrations/2024_04_24_031301_create_assets_table.php
+++ database/migrations/2024_04_24_031301_create_assets_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->string('asset_code');
             $table->string('asset_name');
             $table->foreignId('category_id')->constrained('categories')->onDelete("cascade");
-            $table->enum('item_condition',['rusak', 'bagus']);
+            $table->enum('item_condition', range(1, 7));
             $table->bigInteger('price');
             $table->dateTime('received_date');
             $table->dateTime('expiration_date');
-            $table->enum('status', ['aktif', 'tidak_aktif']);
+            $table->enum('status', range(1, 8));
             $table->timestamps();
         });
     }
