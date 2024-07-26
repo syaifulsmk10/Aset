0e89b1d8d8ffc11ccdc688458712e547288684e8
syaifulsmk10 <113504075+syaifulsmk10@users.noreply.github.com>
2024-07-26 14:25:42 +0700

    new

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -316,7 +316,7 @@ class ApplicantController extends Controller
                 "ExpiryDate" => $Applicant->expiry_date,
                 "UserId" => $Applicant->user->id,
                 "UserApplicants" => $Applicant->user->name,
-                "type" => (int) $Applicant->getAttributes()['type'],
+                "type" => $Applicant->type,
                 "Images" => $Applicant->images->map(function ($image) {
                     $data = json_decode($image->path, true);
 
@@ -541,6 +541,7 @@ class ApplicantController extends Controller
             }
 
             $response = $applicant->toArray();
+            $response['type'] = self::getTypeInteger($applicant->type); 
             $response['image_assets'] = [];
 
             foreach ($applicant->images as $image) {
