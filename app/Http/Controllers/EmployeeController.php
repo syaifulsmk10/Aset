1ab5388feeeb9cf96dc831289928c72cee5c8317
syaifulsmk10 <113504075+syaifulsmk10@users.noreply.github.com>
2024-07-29 13:04:07 +0700

    new

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -127,7 +127,7 @@ class EmployeeController extends Controller
         if (Auth::user()->role->id == 1) {
             $validator = Validator::make($request->all(), [
                 'name' => 'sometimes|required|string|max:255',
-                'email' => 'sometimes|required|string|email|max:255|unique:users',
+                'email' => 'sometimes|string|email|max:255|unique:users',
                 'password' => 'sometimes|required|string|min:8',
                 'nip' => 'sometimes|required|integer|unique:employees',
                 'department_id' => 'sometimes|required|exists:departments,id',
@@ -148,9 +148,13 @@ class EmployeeController extends Controller
             if ($request->has('name')) {
                 $user->name = $request->name;
             }
+
             if ($request->has('email')) {
                 $user->email = $request->email;
+            }else{
+                $user->email = $user->email;
             }
+
             if ($request->has('password')) {
                 $user->password = Hash::make($request->password);
             }
