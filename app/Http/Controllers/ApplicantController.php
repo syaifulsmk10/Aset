16c43e6636b3543862b1f2bac2e21d97503f824d
syaifulsmk10 <113504075+syaifulsmk10@users.noreply.github.com>
2024-07-29 15:39:53 +0700

    new

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -83,9 +83,7 @@ class ApplicantController extends Controller
                 $query->where('user_id', $userId)
                     ->whereNotNull('accepted_at')
                     ->where('type', 1)
-                }) ->with(['applicants' => function($query) {
-                    $query->orderBy('updated_at', 'desc');
-                }])
+                }) // Status "Dipinjamkan"
                 ->get();
         } else {
             return response()->json(['message' => 'Invalid transaction type'], 400);
